e50f3c8ec39de2ee69d29586cef25ec08a5d5299
Qingrui Hu <invoker-bot@outlook.com>
2023-12-29 00:57:49 +0800

    Fix incorrect folder compression

--- tests/test_filecmp.py
+++ tests/test_filecmp.py
@@ -4,24 +4,37 @@
 import filecmp
 import tempfile
 from os import path
-from webdriver_browser.patch import pack_dir_with_ref, unpack_dir_with_ref, generate_dir_hash
+from webdriver_browser.patch import pack_dir_with_ref, unpack_dir_with_ref, generate_dir_hash, generate_dir_diff_with_ref
 
 
 current_dir = path.dirname(__file__)
 
 
 def test_filecmp():
-    assert filecmp.cmp(path.join(current_dir, "default", "same.txt"), path.join(current_dir, "other", "same.txt"))
-    assert not filecmp.cmp(path.join(current_dir, "default", "diff.txt"), path.join(current_dir, "other", "diff.txt"), shallow=False)
+    assert filecmp.cmp(path.join(current_dir, "default", "same.txt"),
+                       path.join(current_dir, "other", "same.txt"), shallow=False)
+    assert not filecmp.cmp(path.join(current_dir, "default", "diff.txt"),
+                           path.join(current_dir, "other", "diff.txt"), shallow=False)
+
+
+def test_generate_dir_diff_with_ref():
+    diff = generate_dir_diff_with_ref(path.join(current_dir, 'default'), path.join(current_dir, 'other'))
+    assert set(diff["left_only"].keys()) == {path.join('.', 'left.txt'),
+                                             path.join('.', 'left'), path.join('.', 'folder', 'empty')}
+    assert set(diff["right_only"].keys()) == {path.join('.', 'right.txt'), path.join(
+        '.', 'right'), path.join('.', 'right', 'right.txt'), path.join('.', 'folder', 'null')}
+    assert set(diff["common"].keys()) == {path.join('.', 'folder'), path.join(
+        '.', 'same.txt'), path.join('.', 'same'), path.join('.', 'same', 'same.txt')}
+    assert set(diff["diff_files"].keys()) == {path.join('.', 'diff.txt')}
 
 
 def test_dircmp():
     diffs = filecmp.dircmp(path.join(current_dir, "default"), path.join(current_dir, "other"))
-    assert set(diffs.left_only) == {'left.txt'}
-    assert set(diffs.right_only) == {'right.txt'}
+    assert set(diffs.left_only) == {'left', 'left.txt'}
+    assert set(diffs.right_only) == {'right', 'right.txt'}
     assert set(diffs.diff_files) == {'diff.txt'}
     assert set(diffs.same_files) == {'same.txt'}
-    assert set(diffs.common) == {'diff.txt', 'same.txt'}
+    assert set(diffs.common_dirs) == {'same', 'folder'}
 
 
 def test_zip():
@@ -34,5 +47,5 @@ def test_zip():
     assert len(diffs.right_only) == 0
     assert len(diffs.diff_files) == 0
     assert set(diffs.same_files) == {'diff.txt', 'same.txt', 'right.txt'}
-    assert set(diffs.common) == {'diff.txt', 'same.txt', 'right.txt'}
+    assert set(diffs.common_files) == {'diff.txt', 'same.txt', 'right.txt'}
     assert generate_dir_hash(path.join(current_dir, "other")) == generate_dir_hash(tmp_dir)
--- webdriver_browser/patch.py
+++ webdriver_browser/patch.py
@@ -11,7 +11,7 @@ from .rsync import blockchecksums, rsyncdelta, patchstream
 def generate_dir_hash(directory: str):
     """Generate a hash of a directory."""
     hash_dict = {}
-    for root, _dirs, files in walk(directory):
+    for root, dirs, files in walk(directory):
         relroot = path.relpath(root, directory)
         for file in files:
             fullpath = path.join(root, file)
@@ -20,16 +20,19 @@ def generate_dir_hash(directory: str):
                 sha = hashlib.sha256(f.read()).hexdigest()
             relpath = path.join(relroot, file)
             hash_dict[relpath] = [sha, size]
+        for dir_ in dirs:
+            relpath = path.join(relroot, dir_)
+            hash_dict[relpath] = None
     return hash_dict
 
 
-def pack_dir_with_ref(ref_dir: str, zip_path: str, src_dir: str, remove=True):
-    """Pack a directory with a reference directory."""
+def generate_dir_diff_with_ref(ref_dir: str, src_dir: str, relpath="."):
+    """Generate a diff of a directory with a reference directory."""
     diff_with_ref = {
         "left_only": {
 
         },
-        "same_files": {
+        "common": {
 
         },
         "right_only": {
@@ -39,20 +42,45 @@ def pack_dir_with_ref(ref_dir: str, zip_path: str, src_dir: str, remove=True):
 
         },
     }
-    comparsion = filecmp.dircmp(ref_dir, src_dir)
+    ref_dir_start = path.join(ref_dir, relpath)
+    src_dir_start = path.join(src_dir, relpath)
+    comparsion = filecmp.dircmp(ref_dir_start, src_dir_start)
     for file in comparsion.left_only:
-        diff_with_ref["left_only"][file] = path.isdir(path.join(ref_dir, file))
+        diff_with_ref["left_only"][path.join(relpath, file)] = None if path.isdir(path.join(ref_dir_start, file)) else True
     for file in comparsion.same_files:
-        diff_with_ref["same_files"][file] = path.isdir(path.join(ref_dir, file))
+        diff_with_ref["common"][path.join(relpath, file)] = None if path.isdir(path.join(ref_dir_start, file)) else True
     for file in comparsion.right_only:
-        with open(path.join(src_dir, file), "rb") as f:
-            diff_with_ref["right_only"][file] = f.read()
+        right_path = path.join(src_dir_start, file)
+        if path.isdir(right_path):
+            diff_with_ref["right_only"][path.join(relpath, file)] = None
+            for root, dirs, files in walk(right_path):
+                for file in files:
+                    fullpath = path.join(root, file)
+                    with open(fullpath, "rb") as f:
+                        diff_with_ref["right_only"][path.join(relpath, path.relpath(fullpath, src_dir_start))] = f.read()
+                for dir_ in dirs:
+                    fullpath = path.join(root, dir_)
+                    diff_with_ref["right_only"][path.join(relpath, path.relpath(fullpath, src_dir_start))] = None
+        elif path.isfile(right_path):
+            with open(right_path, "rb") as f:
+                diff_with_ref["right_only"][path.join(relpath, file)] = f.read()
     for file in comparsion.diff_files:
-        with open(path.join(ref_dir, file), "rb") as f:
+        with open(path.join(ref_dir_start, file), "rb") as f:
             ref_file = blockchecksums(f)
-        with open(path.join(src_dir, file), "rb") as f:
+        with open(path.join(src_dir_start, file), "rb") as f:
             delta = rsyncdelta(f, ref_file)
-            diff_with_ref["diff_files"][file] = delta
+        diff_with_ref["diff_files"][path.join(relpath, file)] = delta
+    for folder in comparsion.common_dirs:
+        diff_with_ref["common"][path.join(relpath, folder)] = None
+        sub_diff_with_ref = generate_dir_diff_with_ref(ref_dir, src_dir, path.join(relpath, folder))
+        for key, value in diff_with_ref.items():
+            value.update(sub_diff_with_ref[key])
+    return diff_with_ref
+
+
+def pack_dir_with_ref(ref_dir: str, zip_path: str, src_dir: str, remove=True):
+    """Pack a directory with a reference directory."""
+    diff_with_ref = generate_dir_diff_with_ref(ref_dir, src_dir)
     diff_with_ref["hash"] = generate_dir_hash(ref_dir)
     with open(zip_path, "wb") as f:
         f.write(zlib.compress(pickle.dumps(diff_with_ref)))
@@ -60,32 +88,36 @@ def pack_dir_with_ref(ref_dir: str, zip_path: str, src_dir: str, remove=True):
         shutil.rmtree(src_dir)
 
 
-def unpack_dir_with_ref(ref_dir: str, zip_path: str, dst_dir: str, remove=True):
+def unpack_dir_with_ref(ref_dir: str, zip_path: str, dist_dir: str, remove=True):
     """Unpack a directory with a reference directory."""
     with open(zip_path, "rb") as f:
         diff_with_ref = pickle.loads(zlib.decompress(f.read()))
     hash_dict = diff_with_ref.get("hash", None)
     if hash_dict is not None:
         if generate_dir_hash(ref_dir) != hash_dict:
-            print(generate_dir_hash(ref_dir))
             raise ValueError(f"reference directory '{ref_dir}' changed")
-    makedirs(dst_dir, exist_ok=True)
-    for file, is_dir in diff_with_ref["same_files"].items():
-        if is_dir:
-            makedirs(path.join(dst_dir, file), exist_ok=True)
+    makedirs(dist_dir, exist_ok=True)
+    for file, is_file in diff_with_ref["common"].items():
+        if is_file is not None:
+            makedirs(path.dirname(path.join(dist_dir, file)), exist_ok=True)
+            shutil.copy(path.join(ref_dir, file), path.join(dist_dir, file), follow_symlinks=False)
         else:
-            shutil.copy(path.join(ref_dir, file), path.join(dst_dir, file), follow_symlinks=False)
-
+            makedirs(path.join(dist_dir, file), exist_ok=True)
     for file, data in diff_with_ref["right_only"].items():
-        with open(path.join(dst_dir, file), "wb") as f:
-            f.write(data)
+        if data is not None:
+            makedirs(path.dirname(path.join(dist_dir, file)), exist_ok=True)
+            with open(path.join(dist_dir, file), "wb") as f:
+                f.write(data)
+        else:
+            makedirs(path.join(dist_dir, file), exist_ok=True)
     for file, delta in diff_with_ref["diff_files"].items():
         with open(path.join(ref_dir, file), "rb") as ref_file:
-            with open(path.join(dst_dir, file), "wb") as f:
+            makedirs(path.dirname(path.join(dist_dir, file)), exist_ok=True)
+            with open(path.join(dist_dir, file), "wb") as f:
                 patchstream(ref_file, f, delta)
 
-    for file, is_dir in diff_with_ref["left_only"].items():
-        p = path.join(dst_dir, file)
+    for file in diff_with_ref["left_only"].keys():
+        p = path.join(dist_dir, file)
         if path.exists(p):
             if path.isdir(p):
                 shutil.rmtree(p)
